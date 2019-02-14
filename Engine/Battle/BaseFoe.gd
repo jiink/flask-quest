f28fdfa76dc673aa6d277634326825ce884caa58
Jacob <jacob.j.carlson03@gmail.com>
2019-02-14 13:05:24 -0800

    ditto

--- Engine/Battle/BaseFoe.gd
+++ Engine/Battle/BaseFoe.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
-var max_hp = 100
-var hp = max_hp
+signal foe_died
+
+var max_hp
+var hp
 
 var damage = 15
 
 func _ready():
-	add_to_group("foes")
+	max_hp = 100
+	hp = max_hp
+	
+	connect("foe_died", get_node("/root/BattleScene"), "foe_died")
+	update_hp_label()
+	
+func get_hurt(damage):
+	hp -= damage
+	update_hp_label()
+	
+	if hp <= 0:
+		die()
+		
+func die():
+	emit_signal("foe_died")
+	queue_free()
+
+func update_hp_label():
+	$HPLabel.text = str(str(hp), " / ", str(max_hp))
