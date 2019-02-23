c5d980e0a09084d0366352e38174ea3410bd4ef7
Jacob <jacob.j.carlson03@gmail.com>
2019-02-23 15:01:41 -0800

    Added test warp portals, scene switching, and a scene transition scene

--- Engine/Singletons/global.gd
+++ Engine/Singletons/global.gd
@@ -3,6 +3,8 @@ extends Node
 # player vars
 var player_hp = 100
 
+################## battle ##################
+
 # battle vars
 var initial_enemies = []
 var current_enemies = initial_enemies
@@ -14,6 +16,27 @@ func start_battle(foes):
 	#print(get_tree().get_current_scene())
 	#print(get_tree().get_current_scene().get_path())
 #	prev_scene = (str(get_tree().get_current_scene().get_path()) + ".tscn").replace("/root", "res:/")
-	prev_scene = get_tree().get_current_scene().filename
+	
+	var battle_scene = load("res://Engine/Battle/BattleScene.tscn")
+	get_tree().get_root().add_child(battle_scene)
+	
+	prev_scene = load(get_tree().get_current_scene().filename)
 	print(prev_scene)
-	get_tree().change_scene("res://Engine/Battle/BattleScene.tscn")
+	#get_tree().change_scene("res://Engine/Battle/BattleScene.tscn")
+	get_tree().get_root().remove_child(prev_scene)
+
+
+################## scene ##################
+
+var player_new_position
+
+func switch_scenes(new_scene, new_player_position):
+	print("changing scenes")
+	get_tree().change_scene(new_scene)
+	
+	yield(get_tree().create_timer(0.01), "timeout")
+	
+	if new_player_position != null:
+		get_tree().get_nodes_in_group("Player")[0].position = new_player_position
+	else:
+		print("warning: new player pos is null")
---
+++ Rooms/Test/Assets/WarpPortal.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+export (String, FILE, "*.tscn") var new_scene = ""
+export (Vector2) var player_new_position = null
+
+func _ready():
+	$Area2D.connect("body_entered", self, "on_body_entered")
+	$AnimationPlayer.play("normal")
+	
+func on_body_entered(body):
+	print(str(body.name) + " entered portal")
+	
+	if new_scene != "":
+		#get_tree().change_scene(new_scene)
+		get_node("/root/global").switch_scenes(new_scene, player_new_position)
+	else:
+		print("error: new_scene empty")
