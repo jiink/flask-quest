8f608325914763932cc1fabd17d65bcca2a82981
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-04-19 01:07:08 -0700

    Bullets have 3 types (nrml, grn, & orng), and hurt the player

--- Engine/Battle/Dodger/Projectiles/Bullet/Bullet.gd
+++ Engine/Battle/Dodger/Projectiles/Bullet/Bullet.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
+#TODO: MAKE BULLET DESTROY SELF!!!!!!!!!!11
+
 export(bool) var face_center = false
 export(NodePath) var face_node
+enum {NORMAL, GREEN, ORANGE}
+export(int, "NORMAL", "GREEN", "ORANGE") var type
+export(int) var damage = 15
 
 export(float) var speed = 100
 export(float) var speed_change = 1
@@ -9,6 +14,8 @@ export(float) var speed_change = 1
 var vec
 
 func _ready():
+	connect("area_entered", self, "area_entered")
+	
 	if face_center:
 		if get_parent().name == "DodgerField":
 			look_at(get_parent().position)
@@ -22,6 +29,31 @@ func _ready():
 	
 	vec = Vector2(speed, 0).rotated(rotation)
 	
+	match type:
+		NORMAL:
+			pass
+		GREEN:
+			set_modulate(Color("72D031"))
+		ORANGE:
+			set_modulate(Color("F68F31"))
+		_:
+			print("Error: Invalid projectile type; setting to NORMAL")
+			type = NORMAL
+	
+	
 func _process(delta):
 	position += vec * delta
 	vec *= speed_change
+	
+func area_entered(area):
+	print("something was hit...")
+	if area.get_parent().name == "GreenSprite" and type != GREEN:
+		print("... it was green")
+		if has_node("../../.."):
+			get_node("../../..").call("hurt", "green", damage)
+		queue_free()
+	elif area.get_parent().name == "OrangeSprite" and type != ORANGE:
+		print("... it was orange")
+		if has_node("../../.."):
+			get_node("../../..").call("hurt", "orange", damage)
+		queue_free()
