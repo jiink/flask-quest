2832fc1b2313b3c588c68df2c5a743a7efda8fe9
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-04-18 23:15:27 -0700

    Now you can move G&O on the dodger-game

---
+++ Engine/Battle/Dodger/DodgerField.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+var rot_green =   0.0
+var rot_green_v = 0.0
+
+var rot_orange =   180.0
+var rot_orange_v = 0.0
+
+var rot_speed = 0.5
+var max_rot_speed = 3.0
+var rot_friction = 0.7
+
+func _process(delta):
+	
+	rot_green_v =  clamp(rot_green_v, -max_rot_speed, max_rot_speed)
+	if Input.is_action_pressed("left"):
+		rot_green_v -= rot_speed
+	elif Input.is_action_pressed("right"):
+		rot_green_v += rot_speed
+	else:
+		rot_green_v *= rot_friction
+		
+	rot_orange_v = clamp(rot_orange_v, -max_rot_speed, max_rot_speed)
+	if Input.is_action_pressed("a"):
+		rot_orange_v -= rot_speed
+	elif Input.is_action_pressed("b"):
+		rot_orange_v += rot_speed
+	else:
+		rot_orange_v *= rot_friction
+	
+	rot_green += rot_green_v
+	rot_orange += rot_orange_v
+	
+	$GreenDodger.set_rotation_degrees(rot_green)
+	$OrangeDodger.set_rotation_degrees(rot_orange)
