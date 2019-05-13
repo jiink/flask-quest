2eb83242214127bafa66838ce80e361435342884
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-05-12 23:26:19 -0700

    you can now start battles with any enemies in them

--- Engine/Singletons/global.gd
+++ Engine/Singletons/global.gd
@@ -30,7 +30,8 @@ func custompause(node, x):
 			print("- "+N.get_name())
 
 func start_battle(foes):
-	initial_enemies = ["Boque", "Boque", "Boque"]
+	initial_enemies = foes
+#	initial_enemies = ["Boque", "Boque", "Boque"]
 	#print(get_tree().get_current_scene())
 	#print(get_tree().get_current_scene().get_path())
 #	prev_scene = (str(get_tree().get_current_scene().get_path()) + ".tscn").replace("/root", "res:/")
--- NPC/Boque/Boque.gd
+++ NPC/Boque/Boque.gd
@@ -14,8 +14,11 @@ func test_react():
 func test_react2(foo, x, y):
 	print(foo + str(x + y))
 	
-func enter_battle():
-	print("battle time")
-	get_node("/root/global").start_battle(["Boque", "Boque", "Boque"])
+func enter_battle(foes):
+	if foes != null:
+		get_node("/root/global").start_battle(foes)
+	else:
+		get_node("/root/global").start_battle(["Boque"])
 	#get_node("/root/global").initial_enemies = ["Boque", "Boque", "Boque"]
 	#get_tree().change_scene("res://Engine/Battle/BattleScene.tscn")
+	print("battle time")
