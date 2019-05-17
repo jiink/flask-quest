b37c957be7448fbe6add1b208cb89e093ffdb016
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-05-16 23:31:44 -0700

    effects have a duration now (also there's a fire effect)

--- Engine/Battle/StatusEffects/fire.gd
+++ Engine/Battle/StatusEffects/fire.gd
@@ -1,12 +1,14 @@
 extends Node
 
 var level = 1
+var duration
 var damage
 var parent
 
 func _ready():
-	print("Hi, I'm %s, and I now exist" % name)
+#	print("Hi, I'm %s, and I now exist" % name)
 	damage = 5 * level 
+	duration = level
 	parent = get_parent()
 	if parent.get_class() == "Node2D":
 		parent.set_modulate(Color(1, 0, 0, 1))
@@ -14,8 +16,13 @@ func _ready():
 		print("Error, error: status effect parent invalid. Parent is %s" % parent.get_class())
 
 func do_effect():
-	print("Hi i'm a status effect and now i'm doing my thing")
+#	print("Hi i'm a status effect and now i'm doing my thing")
 	parent.get_hurt(damage + randi() % 10)
+	
+	duration -= 1
+	if duration <= 0:
+		relief()
 
 func relief():
 	parent.set_modulate(Color(1, 1, 1, 1))
+	queue_free()
