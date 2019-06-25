80c391a64603777e00a56e55d5b2b9c7003fd403
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-06-25 01:27:10 -0700

    give_item was supposed to be all about the inventory not the loadout! oops!

--- Engine/Items/ItemManager.gd
+++ Engine/Items/ItemManager.gd
@@ -2,18 +2,18 @@ extends "res://Engine/Items/ItemDB.gd"
 
 var inventory = ['stone', 'stick', 'stick']
 var loadout = ['base_potion', 'fire_chemical', 'base_potion']
-var loadout_capacity = 24
+var inventory_capacity = 24
 
 func give_item(the_item):
 	var player = get_tree().get_nodes_in_group("Player")[0]
-	if loadout.size() + 1 > loadout_capacity:
+	if inventory.size() + 1 > inventory_capacity:
 		if player:
-			player.do_floaty_text("Loadout full!")
-		print("Loadout full")
+			player.do_floaty_text("Inventory full!")
+		print("Inventory full")
 	else:
-		loadout.append(the_item)
+		inventory.append(the_item)
 		if player:
-			player.do_floaty_text("Obtained %s!" % the_item)
+			player.do_floaty_text("Obtained %s!" % items[the_item].name)
 
 func _ready():
 	pass
