44bf8510f37a99f464d817b3f154f66b75060d94
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-06-25 00:34:30 -0700

    added give_item function to uuuhhhhh give items

--- Engine/Items/ItemManager.gd
+++ Engine/Items/ItemManager.gd
@@ -2,6 +2,18 @@ extends "res://Engine/Items/ItemDB.gd"
 
 var inventory = ['stone', 'stick', 'stick']
 var loadout = ['base_potion', 'fire_chemical', 'base_potion']
+var loadout_capacity = 24
+
+func give_item(the_item):
+	var player = get_tree().get_nodes_in_group("Player")[0]
+	if loadout.size() + 1 > loadout_capacity:
+		if player:
+			player.do_floaty_text("Loadout full!")
+		print("Loadout full")
+	else:
+		loadout.append(the_item)
+		if player:
+			player.do_floaty_text("Obtained %s!" % the_item)
 
 func _ready():
 	pass
