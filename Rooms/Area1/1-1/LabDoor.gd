cf55d9ca1c2e594eae766986da9258c33c37a574
Jacob <jacob.j.carlson03@gmail.com>
2019-07-01 15:58:10 -0700

    set up lab door dialogue

---
+++ Rooms/Area1/1-1/LabDoor.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func interact():
+	
+	if not get_owner().lab_door_open:
+		if ItemManager.inventory.has("miniman_item"):
+			DiagHelper.start_talk(self, "IfYesMiniman")
+		else:
+			DiagHelper.start_talk(self, "IfNoMiniman")
+
+func toss_miniman():
+	print("OK OK OK OK OK OK ok miniman miniman lets pretend miniman was thrown")
