6cb0a11579b583f0ee5de89b546005d1dd51add0
Jacob <jacob.j.carlson03@gmail.com>
2019-07-01 16:33:14 -0700

    fixed dialogue not working if you started from main menu

--- Engine/dialogue/DiagHelper.gd
+++ Engine/dialogue/DiagHelper.gd
@@ -1,6 +1,5 @@
 extends Node
 
-onready var diag = get_tree().get_current_scene().get_node("HUD/Diag")
-
 func start_talk(obj, starting_branch = null):
+	var diag = get_tree().get_current_scene().get_node("HUD/Diag")
 	diag.start_talk(obj, starting_branch)
