727ffc85f867afe9f5c47202099d3fa905d11e3e
Jacob <jacob.j.carlson03@gmail.com>
2019-07-02 14:05:11 -0700

    added a transition for the portals used in doors and stuff

--- Rooms/Tools/PositionPortal.gd
+++ Rooms/Tools/PositionPortal.gd
@@ -13,7 +13,9 @@ func on_body_entered(body):
 		if has_node("Position2D"):
 			print("%s -> %s" % [body.position, $Position2D.get_global_transform()[2]])
 			body.position = $Position2D.get_global_transform()[2]
+			get_owner().get_node("HUD/PositionTransition").go()
 		elif player_new_position != null:
 			body.position = player_new_position.position
+			get_owner().get_node("HUD/PositionTransition").go()
 		else:
 			print("error: teleportal doesn't have destination")
