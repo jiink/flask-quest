84d869f96bd53b924ae91aeef05e463a15c3f561
Jacob <jacob.j.carlson03@gmail.com>
2019-08-01 14:07:45 -0700

    gave elevator-room a door opening animation, fixed green's lightoccluder, and fixed the elevator floor label

--- Rooms/Area1/1-1/ElevatorRoom.gd
+++ Rooms/Area1/1-1/ElevatorRoom.gd
@@ -6,3 +6,4 @@ extends Node2D
 func _on_TriggerField_body_entered(body):
 	if body == get_tree().get_nodes_in_group("Player")[0]:
 		$AnimationPlayer.play("godown")
+		$elevator_floor_indicator.set_process(true)
--- Rooms/Area1/1-1/elevator_floor_indicator.gd
+++ Rooms/Area1/1-1/elevator_floor_indicator.gd
@@ -2,5 +2,8 @@ extends Sprite
 
 export(int) var floornum = 255
 
+func _ready():
+	set_process(false)
+
 func _process(delta):
 	$Label.text = str(floornum)
