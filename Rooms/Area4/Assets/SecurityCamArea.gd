2e5f8554976a9a2e95265c3bb3a0d8df0051e901
Kyle <kyle.jon.carlson@gmail.com>
2019-10-14 17:31:45 -0700

    Added a lot of details in 4-1, added lighting, collisions, etc. in 4-2

---
+++ Rooms/Area4/Assets/SecurityCamArea.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var security_chasers_scene = preload("res://Rooms/Area4/Assets/SecurityChasers.tscn")
+
+
+
+func _on_SecurityCamArea_body_entered(body):
+	if body == global.get_player():
+		print("%s entered THE CAMERA VIEW" % body.name)
+		body.frozen = true
+		
+		var security_chasers = security_chasers_scene.instance()
+		body.add_child(security_chasers)
