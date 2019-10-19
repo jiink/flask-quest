833fb43285bc9484d6200efcf537143021b60762
Kyle <kyle.jon.carlson@gmail.com>
2019-10-19 10:47:14 -0700

    Added part of Glasstown, added Glasstown tileset, added/changed minor details in 4-1 and 4-2, made bathroom doors in 4-1 work

---
+++ Rooms/Area5/Assets/bg_glasstown_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="bg_glasstown_01" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../Area5/Assets/bg_glasstown_01.png" width="512" height="512"/>
+</tileset>
