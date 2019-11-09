1ecfdc5622e2331ff5ef616dd30394d7f69424f7
Kyle <kyle.jon.carlson@gmail.com>
2019-11-09 14:40:31 -0800

    Added a sofa in front of Stercus's office, added a Glasstown preview, added a Poppyhart preview, added a Glasstown refinery preview

---
+++ Rooms/Area6/Assets/bg_poppyhart_01.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="bg_poppyhart_01" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="bg_poppyhart_01.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="dark_grass" tile="-1"/>
+  <terrain name="med_grass" tile="0"/>
+  <terrain name="dark_grass_detail" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="9" terrain="0,0,0,"/>
+ <tile id="10" terrain="0,0,,0"/>
+ <tile id="32" terrain=",0,,0"/>
+ <tile id="33" terrain="0,0,0,0"/>
+ <tile id="34" terrain="0,,0,"/>
+ <tile id="41" terrain="0,,0,0"/>
+ <tile id="42" terrain=",0,0,0"/>
+ <tile id="64" terrain=",0,,"/>
+ <tile id="65" terrain="0,0,,"/>
+ <tile id="66" terrain="0,,,"/>
+ <tile id="128" terrain=",,,1"/>
+ <tile id="129" terrain=",,1,1"/>
+ <tile id="130" terrain=",,1,"/>
+ <tile id="160" terrain=",1,,1"/>
+ <tile id="161" terrain="1,1,1,1"/>
+ <tile id="162" terrain="1,,1,"/>
+ <tile id="192" terrain=",1,,"/>
+ <tile id="193" terrain="1,1,,"/>
+ <tile id="194" terrain="1,,,"/>
+ <tile id="265" terrain="1,1,1,"/>
+ <tile id="266" terrain="1,1,,1"/>
+ <tile id="297" terrain="1,,1,1"/>
+ <tile id="298" terrain=",1,1,1"/>
+</tileset>
