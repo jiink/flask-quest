d7457350a0f48e52d352614f98dae025f277e17d
Jacob <jacob.j.carlson03@gmail.com>
2019-11-11 11:30:05 -0800

    battle speech bubble wobbles now

--- Engine/Battle/BattleSpeechBubble.gd
+++ Engine/Battle/BattleSpeechBubble.gd
@@ -5,15 +5,21 @@ onready var timer = $BadWordTimer
 
 var badword_pool = ["simpleton", "neanderthal", "nincompoop", "moron", "brainlet", "idiot", "fool", "nitwit", "greenvillager", "twit", "twat", "cretin", "bonehead", "blockhead", "imbecile", "ignoramus", "dunce", "pinhead", "ninny", "bozo"]
 var badword
+onready var base_pos = rect_position
+var t = 0
 
 func _ready():
-#	timer.text = words
 	badword = badword_pool[0]
+	text = words.replace('*', badword)
 
 func _process(delta):
-	# put '*' in the textbox to make bad word 
-	text = words.replace('*', badword)
+	t += 1
+	if t % 3 == 0:
+		rect_position = Vector2(base_pos.x + sin(t*.04)*2.0, base_pos.y + cos(t*.04+3)*2.0)
+
 
 func _on_BadWordTimer_timeout():
 	badword = badword_pool[rand_range(0, badword_pool.size()-1)]
 	timer.wait_time *= 1.15
+	# put '*' in the textbox to make bad word 
+	text = words.replace('*', badword)
