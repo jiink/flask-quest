e23c12d63ef64b6564337c864bcf6797e3df6958
Jacob <jacob.j.carlson03@gmail.com>
2019-11-10 18:35:49 -0800

    fixed a timer issue

--- NPC/BrainJar/Attacks/BrainJarAttack1.gd
+++ NPC/BrainJar/Attacks/BrainJarAttack1.gd
@@ -1,22 +1,17 @@
 extends Node2D
 
-
-
 func _on_HomingMissile_successful_hit():
-	$Timer.set_wait_time(3.0)
-	print("time to do stuff")
+	$Timer.wait_time = 3.0
+	$Timer.start()
+		
 	$Tween.interpolate_property($BattleSpeechBubble, "modulate",
 	null, Color(1, 1, 1, 0),
-	0.1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	0.2, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
+	
 	$BattleSpeechBubble2.visible = true
 	$Tween.interpolate_property($BattleSpeechBubble2, "modulate",
 	Color(1, 1, 1, 0), Color(1, 1, 1, 1),
-	0.1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	0.2, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
 	
-
-var t = 0
-func _process(delta):
-	t += 1
-	if t%5 == 0: print($Timer.time_left)
