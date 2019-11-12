7c4ea43bbc0582745a31597ea5031103f1718580
Jacob <jacob.j.carlson03@gmail.com>
2019-11-11 20:42:49 -0800

    work on 2nd brainjar attack

---
+++ NPC/BrainJar/Attacks/BrainJarAttack2.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+
+func _on_BrainBullet_successful_hit(player_num, dmg):
+	$Timer.wait_time = 3.0
+	$Timer.start()
+		
+	$Tween.interpolate_property($BattleSpeechBubble, "modulate",
+	null, Color(1, 1, 1, 0),
+	0.2, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
+	
+	$BattleSpeechBubble2.visible = true
+	$Tween.interpolate_property($BattleSpeechBubble2, "modulate",
+	Color(1, 1, 1, 0), Color(1, 1, 1, 1),
+	0.2, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
