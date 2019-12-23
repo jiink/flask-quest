5cf53f4d4537435f96035f68313b40eba0dec4c0
Jacob <jacob.j.carlson03@gmail.com>
2019-12-23 13:50:32 -0800

    updated map machine

--- Rooms/Area2/Assets/MapMachine.gd
+++ Rooms/Area2/Assets/MapMachine.gd
@@ -7,4 +7,22 @@ func interact():
 	map_scene = map_scene.instance()
 	get_owner().get_node("HUD").add_child(map_scene)
 	
-	###############
+
+func _ready():
+	$OnArea.connect("body_entered", self, "on_body_entered")
+	$OnArea.connect("body_exited", self, "on_body_exited")
+	
+	frame = 0
+	
+func on_body_entered(body):
+	if body == global.get_player():
+		$AnimationPlayer.play("turn_on")
+
+func on_body_exited(body):
+	if body == global.get_player():
+		$AnimationPlayer.play("turn_off")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "turn_on":
+		$AnimationPlayer.play("running")
+	
