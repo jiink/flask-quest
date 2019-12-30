9ec95868c95c756db87d9034e5599e464239281b
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-12-29 23:32:43 -0800

    2-1 guy won't give you more than one map-machine credentials anymore

--- Rooms/Area2/Assets/MalusHinter.gd
+++ Rooms/Area2/Assets/MalusHinter.gd
@@ -1,5 +1,11 @@
 extends "res://Engine/BasicNPC.gd"
 
+func interact():
+	if not ("map_login" in ItemManager.inventory):
+		DiagHelper.start_talk(self, "ToGiveCreds")
+	else:
+		DiagHelper.start_talk(self, "AlreadyHasCreds")
+
 func give_credentials():
 	ItemManager.give_item("map_login")
 	
