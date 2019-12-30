a17a9c9eb6e09a8b60e4e07a3bad3c0a4de9b935
Jacob Carlson <jacob.j.carlson03@gmail.com>
2019-12-29 23:30:20 -0800

    renamed "LanettaMap" to "MapDisplay"

--- Rooms/Area2/Assets/MapMachine.gd
+++ Rooms/Area2/Assets/MapMachine.gd
@@ -5,7 +5,7 @@ func interact():
 	if "map_login" in ItemManager.inventory:
 		print("open the map!") # open the map
 		# here we will open the map:
-		var map_scene = load("res://Rooms/Area2/Assets/LanettaMap.tscn")
+		var map_scene = load("res://Rooms/Area2/Assets/MapDisplay.tscn")
 		map_scene = map_scene.instance()
 		get_owner().get_node("HUD").add_child(map_scene)
 	else:
