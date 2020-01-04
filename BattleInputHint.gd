0fdf11ef467019e30f43c213e7515cb0e98a1de7
Jacob Carlson <jacob.j.carlson03@gmail.com>
2020-01-03 23:29:48 -0800

    added support for icons in input-hints, as well as icons for directional keys and spacebar

--- BattleInputHint.gd
+++ BattleInputHint.gd
@@ -11,7 +11,20 @@ func _ready():
 		$Highlight.visible = false
 	
 	# gets first element of action, gets scancode, and turn that scancode into a readable string
-	$Label.text = OS.get_scancode_string(InputMap.get_action_list(action_hint)[0].scancode)
+	var key_string = OS.get_scancode_string(InputMap.get_action_list(action_hint)[0].scancode)
+	var key_icon_path = "res://Engine/InputIcons/"
+	var keys_to_icons = {
+		"Up" : "kb_arrow_up",
+		"Down" : "kb_arrow_down",
+		"Left" : "kb_arrow_left",
+		"Right" : "kb_arrow_right",
+		"Space" : "kb_space",
+	}
+	if keys_to_icons.has(key_string):
+		$Icon.texture = load(key_icon_path + keys_to_icons[key_string] + ".png")
+		$Label.text = "";
+	else:
+		$Label.text = key_string;
 
 func _process(delta):
 	pass
