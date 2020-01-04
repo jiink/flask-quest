ff3d2943ec7bd46a49738691070a37f0fefe009f
Jacob <jacob.j.carlson03@gmail.com>
2020-01-04 14:38:43 -0800

    added more voice options for DiagPieces

--- Engine/dialogue/Diag.gd
+++ Engine/dialogue/Diag.gd
@@ -97,11 +97,18 @@ func update_boxes(new_target):
 		text_time = target_tree.default_text_delay
 	
 	# set sound
-	if target_tree.voice_sound != null:
-		voice_sound = target_tree.voice_sound
+	if not target_piece.no_voice:
+		if target_piece.voice_sound != null:
+			voice_sound = target_piece.voice_sound
+		else:
+			voice_sound = target_tree.voice_sound
 		audio.set_stream(voice_sound)
-	if target_tree.voice_variation != null:
-		voice_variation = clamp(target_tree.voice_variation, 0.0, 0.9)
+		
+		if target_piece.voice_variation != null:
+			voice_variation = target_piece.voice_variation
+		elif target_tree.voice_variation != null:
+			voice_variation = target_tree.voice_variation
+		voice_variation = clamp(voice_variation, 0.0, 0.9)
 	
 	# set font
 	if target_tree.default_font != null:
--- Engine/dialogue/DiagPiece.gd
+++ Engine/dialogue/DiagPiece.gd
@@ -6,6 +6,9 @@ export(float) var text_delay = null
 export(bool) var no_face = false
 export(String, "(none)", "neutral", "openmouth", "sidemouth", "happy", "cute", "sad", "suspicious", "crying", "cryingloud", "grin", "bigsurprise", "biggersurprise", "angry", "misc", "surprise", "stare", "smug") var expression = "(none)"
 export(Texture) var face_texture = null
+export(bool) var no_voice = false
+export(AudioStream) var voice_sound = null
+export(float) var voice_variation = null
 export(String) var function = ""
 export(Array) var args
 export(bool) var skippable = true
