c274a474c857328ee6b2a6ac7888616868dba9ac
Jacob <jacob.j.carlson03@gmail.com>
2020-01-04 16:53:47 -0800

    added a tick system, and made the security mop ai use ticks

--- Engine/FoeFollow.gd
+++ Engine/FoeFollow.gd
@@ -7,31 +7,52 @@ var target_distance
 var chasing = false
 var base_name
 onready var target = global.get_player()
+var angle = 0.0
 
 func _ready():
 	base_name = name.replace("@", "")
 	for i in range(9):
 		base_name = base_name.replace(str(i), "")
 		
-func _process(delta):
+func _tick():
 	if target:
 		target_distance = get_global_transform().origin.distance_to(target.get_global_transform().origin)
 		if target_distance < follow_distance:
 				if not is_touching_player():
 					chasing = true
-					var angle = target.get_global_transform().origin.angle_to_point(get_global_transform().origin)
+					angle = target.get_global_transform().origin.angle_to_point(get_global_transform().origin)
 					$Sprite.flip_h = abs(angle) >= PI*0.5
-					move_and_slide(Vector2(speed, 0).rotated(angle))
+#					move_and_slide(Vector2(speed, 0).rotated(angle))
 				else:
 					if not target.get("invincible"):
 						trigger()
 		else:
 			chasing = false
-		
+
+func _process(delta):
+	if chasing:
+		move_and_slide(Vector2(speed, 0).rotated(angle))
+
+#func _process(delta):
+#	if target:
+#		target_distance = get_global_transform().origin.distance_to(target.get_global_transform().origin)
+#		if target_distance < follow_distance:
+#				if not is_touching_player():
+#					chasing = true
+#					var angle = target.get_global_transform().origin.angle_to_point(get_global_transform().origin)
+#					$Sprite.flip_h = abs(angle) >= PI*0.5
+#					move_and_slide(Vector2(speed, 0).rotated(angle))
+#				else:
+#					if not target.get("invincible"):
+#						trigger()
+#		else:
+#			chasing = false
+#
 			
 func is_touching_player():
 	return $CollisionShape2D.get_global_transform().origin.distance_to(target.get_global_transform().origin) < 16
 
 func trigger():
 	set_process(false)
+	TickManager.set_tick(self, false)
 	$"/root/global".start_battle([base_name])
---
+++ Engine/Singletons/TickManager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var tick_rate = 0.1 # how often a tick happens in seconds
+onready var timer = $TickTimer
+
+func _ready():
+	timer.connect("timeout", self, "on_ticktimer_timeout")
+	timer.start(tick_rate)
+
+func on_ticktimer_timeout():
+	get_tree().call_group("tick", "_tick")
+
+func set_tick(who, choice):
+	if choice == true:
+		who.add_to_group("tick")
+	elif choice == false:
+		who.remove_from_group("tick")
