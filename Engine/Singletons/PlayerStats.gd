8c906f0e489b882c459c303171d997a7c37221d1
Jacob <jacob.j.carlson03@gmail.com>
2020-03-20 11:55:09 -0700

    the save isn't look at for the followers, and PlayerStats doesn't spawn them either

--- Engine/Singletons/PlayerStats.gd
+++ Engine/Singletons/PlayerStats.gd
@@ -2,10 +2,6 @@ extends Node
 
 var player_count = 1 # for "1 player" or "2 player"
 
-var orange = preload("res://Player/Orange.tscn")
-
-var party_members = ["orange"]
-
 var green_max_hp = 100
 var green_hp = 100
 
@@ -14,11 +10,8 @@ var orange_hp = 100
 
 var dollars = 0
 
-func _ready():
-	global.connect("scene_changed", self, "on_scene_change")
-	
-
-func on_scene_change():
-	if global.get_player():
-		if "orange" in party_members:
-			global.get_player().get_parent().call_deferred("add_child", orange.instance())
+enum {
+	HUMAN,
+	BOT,
+	EXTERNAL
+}
