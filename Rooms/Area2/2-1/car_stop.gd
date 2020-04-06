c4fe18c14076788bd0e89fcd9c5121d2cf6212e7
Kyle <kyle.jon.carlson@gmail.com>
2020-04-06 16:46:15 -0700

    Changed up main menu, added more glasstown details, added NPCs in city, etc etc

--- Rooms/Area2/2-1/car_stop.gd
+++ Rooms/Area2/2-1/car_stop.gd
@@ -5,12 +5,18 @@ func _ready():
 	$StopArea.connect("body_exited", self, "on_body_exited")
 
 func on_body_entered(body):
-	if body == global.get_player():
+	if body in get_tree().get_nodes_in_group("Player"):
 		get_node("../../AnimationPlayer").playback_speed = 0
+		$CarStop.play()
+		$EngineAudio.pitch_scale = 0.43
 
 func on_body_exited(body):
-	if body == global.get_player():
+	if body in get_tree().get_nodes_in_group("Player"):
 		$Tween.interpolate_property(get_node("../../AnimationPlayer"), "playback_speed",
 			0,1, 
+			1, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		$Tween.start()
+		$Tween.interpolate_property(($EngineAudio), "pitch_scale",
+			0.43,1, 
 			1, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$Tween.start()
