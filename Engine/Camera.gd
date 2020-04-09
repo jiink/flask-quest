35faa33598f58f1fbc796a64a047cdc957e9ea58
Jacob <jacob.j.carlson03@gmail.com>
2020-04-09 15:38:42 -0700

    added screenshake

--- Engine/Camera.gd
+++ Engine/Camera.gd
@@ -1,7 +1,50 @@
 extends Camera2D
 
 export(bool) var follow_player = true
-
+ 
+func _ready():
+	_start_position = offset
+	_start_rotation = rotation
+	_trauma = 0.0
+ 
 func _process(delta):
 	if follow_player:
 		position = get_node("../YSort/Player").position
+	
+	if Input.is_action_just_pressed("confirm"):
+		add_trauma(20)
+	
+	if _trauma > 0:
+		_decay_trauma(delta)
+		_apply_shake()
+		
+# ------------ screenshake script ------------ 
+# https://twitter.com/_Azza292
+# MIT License
+ 
+export var decay_rate = 0.4
+export var max_roll = 10.0
+export var max_offset = 10
+ 
+var _start_position
+var _start_rotation
+var _trauma
+ 
+ 
+func add_trauma(amount):
+	_trauma = min(_trauma + amount, 1)
+
+func _decay_trauma(delta):
+	var change = decay_rate * delta
+	_trauma = max(_trauma - change, 0)
+ 
+func _apply_shake():
+	var shake = _trauma * _trauma
+	var roll = max_roll * shake * _get_neg_or_pos_scalar()
+	var o_x = max_offset * shake * _get_neg_or_pos_scalar()
+	var o_y = max_offset * shake * _get_neg_or_pos_scalar()
+	offset = _start_position + Vector2(o_x, o_y)
+	rotation = _start_rotation + roll
+ 
+func _get_neg_or_pos_scalar():
+		return rand_range(-1.0, 1.0)
