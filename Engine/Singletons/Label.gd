e53f0b4e8a6694ddfc96cf358129146e47d78238
Jacob <jacob.j.carlson03@gmail.com>
2020-05-31 19:06:55 -0700

    fixed the musicmanager's debug label always running

--- Engine/Singletons/Label.gd
+++ Engine/Singletons/Label.gd
@@ -3,7 +3,16 @@ extends Label
 var focus 
 var to_display
 
+var warning_said = false
+
+func _ready():
+	if not visible:
+		set_process(false)
+
 func _process(delta):
+	if not warning_said:
+		warning_said = true
+		print("HEY THE MUSICMANAGER DEBUG LABEL IS RUNNING!!! is it supposed to be?")
 	to_display = ""
 	for i in range(4):
 		focus = get_owner().get_child(i)
