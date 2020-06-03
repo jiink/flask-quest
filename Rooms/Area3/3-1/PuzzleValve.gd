2dccb08fa94de1229388413bf22b0def86023c80
Kyle <kyle.jon.carlson@gmail.com>
2020-06-03 11:10:33 -0700

    Added more city assets

--- Rooms/Area3/3-1/PuzzleValve.gd
+++ Rooms/Area3/3-1/PuzzleValve.gd
@@ -5,10 +5,10 @@ var turned = false
 signal puzzle_valve_turned
 
 func _ready():
-    connect("puzzle_valve_turned", get_node("../PuzzlePipeLeak"), "_on_puzzle_valve_turned")
+	connect("puzzle_valve_turned", get_node("../PuzzlePipeLeak"), "_on_puzzle_valve_turned")
 
 func interact():
-    if not turned:
-        print("puzzle valve activated")
-        emit_signal("puzzle_valve_turned")
-        turned = true
+	if not turned:
+		print("puzzle valve activated")
+		emit_signal("puzzle_valve_turned")
+		turned = true
