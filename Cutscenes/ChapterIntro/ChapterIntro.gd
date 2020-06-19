631e52d973e10916eda75a5229ef3f2fe343c721
Kyle <kyle.jon.carlson@gmail.com>
2020-06-19 11:57:10 -0700

    Added 'wall edges' to all of the scenes of Area 1, made the chapter 1 intro not break the game, made the scene portal's third variable optional. (Extended the lab tileset and modified the .tres as well.)

--- Cutscenes/ChapterIntro/ChapterIntro.gd
+++ Cutscenes/ChapterIntro/ChapterIntro.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	global.start_scene_switch("res://Rooms/Area2/2-1/2-1.tscn", Vector2(271, 28))
+	global.start_scene_switch("res://Rooms/Area2/2-1/2-1.tscn", Vector2(271, 28), "none")
 	global.swap_scenes()
