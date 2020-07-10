140d22e4f90b39f827d17f08952b56142290958e
Kyle <kyle.jon.carlson@gmail.com>
2020-07-10 16:44:30 -0700

    Added the transistion from 2-5 to Glasstown.

--- Rooms/Area2/2-5/2-5.gd
+++ Rooms/Area2/2-5/2-5.gd
@@ -3,3 +3,15 @@ extends "res://Rooms/TemplateRoom.gd"
 
 func _on_StartTimer_timeout():
 	DiagHelper.start_talk(self)
+	
+func start_stage_1():
+	$SleepStages.play("sleep_stage_1")
+	
+func start_stage_2():
+	$SleepStages.play("sleep_stage_2")
+	
+func start_stage_3():
+	$SleepStages.play("sleep_stage_3")
+	yield(get_tree().create_timer(4), "timeout")
+	global.start_scene_switch("res://Rooms/Area5/5-1/5-1.tscn", Vector2(3243,500), "down")
+	global.swap_scenes()
