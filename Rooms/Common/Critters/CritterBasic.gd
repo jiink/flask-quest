487e0e9f9499ece3693fc3a305dff6248e0fa219
Kyle <kyle.jon.carlson@gmail.com>
2020-08-01 13:28:55 -0700

    Added critters (one squirrel and four birds) that run around. Added critters into the city.

---
+++ Rooms/Common/Critters/CritterBasic.gd
@@ -0,0 +1,49 @@
+extends KinematicBody2D
+
+var travel_vec = Vector2(0.0, 0.0)
+var waiting = false
+var t = 1
+var first_anim_frame = 0
+
+export(int) var speed = 24
+export(float) var direction_time_multiplier = 1
+export(float) var direction_time_adder = 1
+export(float) var wait_time_multiplier = 1
+export(float) var wait_time_adder = 1
+export(bool) var first_frame_is_idle = false
+
+func _ready():
+	$DirectionChangeTimer.connect("timeout", self, "on_directiontimer_timeout")
+	$WaitTimer.connect("timeout", self, "on_waittimer_timeout")
+	on_directiontimer_timeout()
+	if first_frame_is_idle:
+		first_anim_frame = 1
+
+
+func _process(delta):
+	if not waiting:
+		move_and_slide(travel_vec * speed)
+		if travel_vec.x < 0:
+			$Sprite.flip_h = true
+		else:
+			$Sprite.flip_h = false
+			
+	if not waiting:
+		t += 8*delta
+		if t < $Sprite.hframes:
+			$Sprite.frame = t
+		else:
+			$Sprite.frame = first_anim_frame
+			t = 0
+
+func on_directiontimer_timeout():
+	travel_vec = Vector2(randf() - 0.5, randf() - 0.5).normalized()
+	$WaitTimer.start(wait_time_adder + randf() * wait_time_multiplier)
+	print("WAIT TIMER = %f" % $WaitTimer.time_left)
+	waiting = true
+	$Sprite.frame = 0
+	
+func on_waittimer_timeout():
+	waiting = false
+	$DirectionChangeTimer.start(direction_time_adder + randf() * direction_time_multiplier)
+	print("DIRECTION TIMER = %f" % $DirectionChangeTimer.time_left)
