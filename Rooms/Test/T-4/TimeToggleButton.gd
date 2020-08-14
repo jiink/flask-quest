499789d2b1792f1e4cd449ba3207fc72b0c1bb21
Kyle <kyle.jon.carlson@gmail.com>
2020-08-14 12:56:54 -0700

    Added Pizza Man's face and T-4, a day/night cycle test room

---
+++ Rooms/Test/T-4/TimeToggleButton.gd
@@ -0,0 +1,23 @@
+extends Sprite
+
+var toggled = false
+
+var color_day = Color(0xffffffff)
+var color_night = Color(0x2d3158ff)
+
+
+func interact():
+	if toggled == false:
+		toggled = true
+		$"../../CanvasModulate".color = color_night
+		for night_lights in get_tree().get_nodes_in_group("night_lights"):
+			night_lights.visible = true
+		$"../..".level_music = preload("res://Rooms/Area4/Assets/maloffice-02.ogg")
+		MusicManager.update_music("level")
+	else:
+		toggled = false
+		$"../../CanvasModulate".color = color_day
+		for night_lights in get_tree().get_nodes_in_group("night_lights"):
+			night_lights.visible = false
+		$"../..".level_music = preload("res://Rooms/Area4/Assets/maloffice-01.ogg")
+		MusicManager.update_music("level")
