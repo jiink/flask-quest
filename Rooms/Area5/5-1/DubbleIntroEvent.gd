d35b982e1bdb7c0ce5c216ae16e7a2e77aa17c45
Kyle <kyle.jon.carlson@gmail.com>
2020-08-17 20:09:45 -0700

    no message

--- Rooms/Area5/5-1/DubbleIntroEvent.gd
+++ Rooms/Area5/5-1/DubbleIntroEvent.gd
@@ -8,4 +8,10 @@ func _ready():
 	DiagHelper.start_talk(self)
 
 func dubble_walk_by():
+	$CharacterMovement.play("dubble_walk_by")
+
+func door_knock():
+	$DoorKnockAudio.playing = true
 	
+func green_wake():
+	print("**************Let the player move around now.***********")
