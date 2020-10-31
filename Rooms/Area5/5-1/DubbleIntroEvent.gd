ced74332dce847a5d94fd5ca4a40a85484e30f1c
Kyle <kyle.jon.carlson@gmail.com>
2020-10-31 08:52:05 -0700

    Modified the Dubble intro event to move Green and Orange. Post-apoc GT now has fire on some sprites, in the form of palette swapping.

--- Rooms/Area5/5-1/DubbleIntroEvent.gd
+++ Rooms/Area5/5-1/DubbleIntroEvent.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 var SAVE_KEY = "5-1_dubble_intro_event_"
 var occured = false
+var green
+var orange
+var orange_original_controller
 
 func save(save_game):
 	save_game.data[SAVE_KEY + "occured"] = occured
@@ -25,6 +28,11 @@ func prepare_suicide(state): # Places Dubble behind his desk and commits suicide
 		$"ActivateArea/CollisionShape2D".set_deferred("disabled", true)
 		
 func setup():
+	green = global.get_player(1)
+	orange = global.get_player(2)
+	orange_original_controller = orange.controlled_by
+	orange.controlled_by = orange.EXTERNAL
+	green.controlled_by = green.EXTERNAL
 	MusicManager.change_music("res://Rooms/Area5/Assets/ontoglasstown-07.ogg", true, 0)
 	$OverlayAnimator.play("wake_up")
 	yield(get_tree().create_timer(3), "timeout")
@@ -40,26 +48,32 @@ func door_knock():
 
 func green_wake():
 	print("**************Let the player move around now.***********")
-#	Animate the player getting out of bed and give them control
+	$CharacterMovement.play("green_exit_bed")
+	green.controlled_by = green.PERSON
 
 func orange_run_to_window():
-	pass
+	orange.controlled_by = orange.EXTERNAL
+	$CharacterMovement.play("orange_run_to_window")
 #	Orange quickly runs to the window.
 
 func orange_turn():
-	pass
-#	Orange turns while he talks
+	orange.extctrl_facing_direction = orange.Direction.DOWN
 
 func _on_ActivateArea_body_entered(body):
 	if body == global.get_player():
 		$CharacterMovement.play("dubble_enter_room")
 #		Player backs up, Dubble talks w/ Green+Orange
+		$Tween.interpolate_property(global.get_player(1), "position", \
+	 	null, global.get_player(1).position + Vector2(0, -16), 0.7, \
+		Tween.TRANS_LINEAR, Tween.EASE_IN)
+		$Tween.start()
 		yield(get_tree().create_timer(3), "timeout")
 		DiagHelper.start_talk(self, "DubbleTalk")
 		$"ActivateArea/CollisionShape2D".set_deferred("disabled", true)
 
 func dubble_intro_end():
 	occured = true
+	orange.controlled_by = orange_original_controller
 	$CharacterMovement.play("dubble_exit_room")
 #	dubble walks out of room and is transported to behind his desk. Player can now move freely.
 
--- Rooms/Area6/6-0/EscapeSequence.gd
+++ Rooms/Area6/6-0/EscapeSequence.gd
@@ -1,18 +1,32 @@
 extends Area2D
 
-
+onready var green = global.get_player(1)
+onready var orange = global.get_player(2)
+onready var camera = get_tree().get_current_scene().get_node("6-0/Camera")
 func _on_EscapeTrigger_body_entered(body):
 	if body in get_tree().get_nodes_in_group("Player"):
+		green.controlled_by = green.EXTERNAL
+		orange.controlled_by = orange.EXTERNAL
 		body.frozen = true
-		$CollisionShape2D.set_deferred("disabled", true)
 		$"../Camera".follow_player = false
-		
-		####Camera tween
+		$CollisionShape2D.set_deferred("disabled", true)
 		yield(get_tree().create_timer(0.5), "timeout")
+		$Tween.interpolate_property(green, "position", 
+			null, Vector2(350,-15),
+			2, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
+		$Tween.interpolate_property(orange, "position", 
+			null, Vector2(340,0),
+			2, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
 		$Tween.interpolate_property($"../Camera", "position",
 			null, Vector2(299, -49),
 			3, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		$Tween.start()
+
+		####Camera tween
+		yield(get_tree().create_timer(0.5), "timeout")
+		$Tween.interpolate_property($"../Camera", "position",
+			null, Vector2(299, -49),
+			2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 		####
 		
 		yield(get_tree().create_timer(3), "timeout")
@@ -22,8 +36,18 @@ func _on_EscapeTrigger_body_entered(body):
 		DiagHelper.start_talk(self)
 
 func diag_finished():
+	$Tween.interpolate_property(green, "position", 
+		null, Vector2(350,-40),
+		0.1, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
+	$Tween.interpolate_property(orange, "position", 
+		null, Vector2(350,-40),
+		0.1, Tween.TRANS_LINEAR, Tween.TRANS_LINEAR)
+	$Tween.start()
+	yield($Tween, "tween_all_completed")
+	green.visible = false
+	orange.visible = false
 	$"../BusPath/AnimationPlayer".play("bus_go")
 
-	yield(get_tree().create_timer(4), "timeout")
+	yield(get_tree().create_timer(6), "timeout")
 	global.start_scene_switch("res://Rooms/Area6/6-1/6-1.tscn", Vector2(667,-78))
 	global.swap_scenes()
