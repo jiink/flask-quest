869b1f489a5bdae5be3dcd0f5847be2c301b8f56
Kyle <kyle.jon.carlson@gmail.com>
2020-12-14 18:16:00 -0800

    New BBB sprite, new attack for the Malus robot guards, added/improved dialogue throughout the game, fixed the sewer exit sequence with a secret technique. Music for Pizza Pow and the Amazing Cats Club.

--- Rooms/Area3/3-2/3-2.gd
+++ Rooms/Area3/3-2/3-2.gd
@@ -2,6 +2,6 @@ extends "res://Rooms/TemplateRoom.gd"
 
 
 func _on_ExitArea_body_entered(body):
-	if body == $Player:
-		global.start_scene_switch("res://Rooms/Area4/4-1/4-1.tscn", Vector2(120, -216), "down")
+	if body == $PhysicsPlayer:
+		global.start_scene_switch("res://Rooms/Area4/4-1/4-1.tscn", Vector2(120, -216))
 		global.swap_scenes()
--- Rooms/Area3/3-2/WallCollisions.gd
+++ Rooms/Area3/3-2/WallCollisions.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-onready var player = get_node("../Player")
+onready var player = get_node("../PhysicsPlayer")
 
 func _process(delta):
 	position.y = player.position.y
